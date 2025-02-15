ec397600eff6b14d92a5d481b16ff92cbb183d20
Ajay sharma <ajaysharmak2529@gmail.com>
2025-02-15 17:15:02 +0530

    Minnor changes to Application layer and Infrastructure layer

--- HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -1,5 +1,5 @@
 ﻿using AutoMapper;
-using HR.LeaveManagement.Application.Contracts;
+using HR.LeaveManagement.Application.Contracts.Infrastructure;
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Application.DTOs.LeaveRequest.Validators;
 using HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
--- HR.LeaveManagment.Persistence/PersistenceServiceRegistration.cs
+++ HR.LeaveManagment.Persistence/PersistenceServiceRegistration.cs
@@ -16,9 +16,7 @@ namespace HR.LeaveManagement.Persistence
                 );
 
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-            services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
-            services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
-            services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             return services;
         }
