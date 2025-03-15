7d0bd21f95433cc7f051244624850198d6d46709
Ajay sharma <ajaysharmak2529@gmail.com>
2025-03-15 19:18:16 +0530

    Fixed leavetype validation for leaveRequest and optional DateTime for LeaveRequest

--- HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -1,14 +1,20 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Constants;
 using HR.LeaveManagement.Application.Contracts.Infrastructure;
 using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Application.DTOs.LeaveRequest.Validators;
 using HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
 using HR.LeaveManagement.Application.Models;
+using HR.LeaveManagement.Application.Models.Identity;
 using HR.LeaveManagement.Application.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,13 +26,15 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Command
         private readonly IEmailSender _emailSender;
         private readonly IMapper _mapper;
         private readonly IHttpContextAccessor _context;
+        private readonly JwtSetting jwtOptions;
 
-        public CreateLeaveRequestCommandHandler(IUnitOfWork unitOfWork, IEmailSender emailSender, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        public CreateLeaveRequestCommandHandler(IUnitOfWork unitOfWork, IEmailSender emailSender, IMapper mapper, IHttpContextAccessor httpContextAccessor, IOptionsMonitor<JwtSetting> jwtOptions)
         {
             _unitOfWork = unitOfWork;
             _emailSender = emailSender;
             _mapper = mapper;
             _context = httpContextAccessor;
+            this.jwtOptions = jwtOptions.CurrentValue;
         }
         public async Task<BaseCommandResponse> Handle(CreateLeaveRequestCommand request, CancellationToken cancellationToken)
         {
@@ -44,17 +52,22 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Command
 
             var leaveRequest = _mapper.Map<Domain.LeaveRequest>(request.LeaveRequestDto);
 
-            if (string.IsNullOrEmpty(_context.HttpContext.Request.Headers["Authorization"]))
+            if (!string.IsNullOrEmpty(_context.HttpContext.Request.Headers["Authorization"].ToString()))
             {
                 var bearerToken = _context.HttpContext.Request.Headers["Authorization"].ToString();
-                var token = bearerToken.Substring(bearerToken.IndexOf("Bearer "));
 
-                var claims = new JwtSecurityTokenHandler().ReadJwtToken(token).Claims;
+                var token = bearerToken.Substring("Bearer ".Length).Trim();
 
-                //leaveRequest = await _unitOfWork.LeaveRequests.AddAsync(leaveRequest);
-                //await _unitOfWork.SaveChangesAsync();
+                var handler = new JwtSecurityTokenHandler();
+                var userId = handler.ReadJwtToken(token).Claims.FirstOrDefault(x => x.Type == CustomClaimTypes.Uid)?.Value;
 
-                //response.Id = leaveRequest.Id;
+                leaveRequest.EmployeeId = userId!;
+                leaveRequest.Approved = false;
+                leaveRequest.CreatedBy = userId!;
+                leaveRequest = await _unitOfWork.LeaveRequests.AddAsync(leaveRequest);
+                await _unitOfWork.SaveChangesAsync();
+
+                response.Id = leaveRequest.Id;
                 response.Success = true;
                 response.Message = "Leave Request Created Successfully";
 
@@ -62,11 +75,12 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Command
                 {
                     Body = $"Your leave request for {leaveRequest.StartDate:D} to {leaveRequest.EndDate:D} is pending approval",
                     Subject = "Leave Request Application",
-                    To = "admin@localhost"
+                    To = "admin@localhost",
+                    IsHtml = false,
                 };
                 try
                 {
-                    await _emailSender.SendEmailAsync(email);
+                    //await _emailSender.SendEmailAsync(email);
                 }
                 catch (System.Exception ex)
                 {
