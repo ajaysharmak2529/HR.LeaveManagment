32a8421c93dc51d1ba27b56ad41435b0b9ada197
Ajay sharma <ajaysharmak2529@gmail.com>
2025-03-26 21:25:21 +0530

    Implemented Admin leave allocations Request and Handler

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Handlers/Queries/GetAdminAllocationListRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.DTOs.LeaveAllocation;
+using HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Queries;
+using HR.LeaveManagement.Application.Models;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Handlers.Queries
+{
+    public class GetAdminAllocationListRequestHandler : IRequestHandler<GetAdminAllocationListRequest, PageList<AllocationGroupResultDto>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public GetAdminAllocationListRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
+        public async Task<PageList<AllocationGroupResultDto>> Handle(GetAdminAllocationListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveAllocationsList = await unitOfWork.LeaveAllocations.GetAllAdminLeaveAllocationsWithDetailAsync( request.Page!.Value, request.PageSize!.Value);
+
+
+            return new PageList<AllocationGroupResultDto>(leaveAllocationsList.Items!, leaveAllocationsList.Page,leaveAllocationsList.PageSize,leaveAllocationsList.TotalCount);
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/Features/LeaveAllocations/Requests/Queries/GetAdminAllocationListRequest.cs
@@ -0,0 +1,12 @@
+﻿using HR.LeaveManagement.Application.DTOs.LeaveAllocation;
+using HR.LeaveManagement.Application.Models;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Queries
+{
+    public class GetAdminAllocationListRequest : IRequest<PageList<AllocationGroupResultDto>>
+    {
+        public int? Page { get; set; }
+        public int? PageSize { get; set; }
+    }
+}
